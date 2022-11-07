4433a8f90c9c43c539246ddf44c01a02fe4cb742
yusha7 <sufyananderson@gmail.com>
2022-11-06 22:02:46 -0500

    projects

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+    'url' : 'mongodb+srv://sufyanA21:demo@cluster0.h7vdga4.mongodb.net/loginInfo?retryWrites=true&w=majority', 
+    'dbName': 'loginInfo'
+};
+
+//TlIYwT0tX9YNfWZW
